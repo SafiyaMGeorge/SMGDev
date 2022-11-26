67029e0521c2911ba3b63033d64106bb0d9889b3
“Safiya <s.m.George@hotmail.com>
2022-11-25 22:12:41 -0400

    Removed event listener because the animation would not execute when the snake was close to the bunny.

--- Projects/BunnyHop_Game/brain/play.js
+++ Projects/BunnyHop_Game/brain/play.js
@@ -2,6 +2,7 @@
 let character = document.getElementById("character");
 let snake = document.getElementById("obstacle");
 //This function will allow the user to control when the bunny character will jump to escape the snake.
+
 function jump(){
     //accessing the class list of the character
     character.classList.add("animate");
@@ -9,4 +10,4 @@ function jump(){
     setTimeout(function(){
         character.classList.remove("animate");
     },900);
-}
+};
