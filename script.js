ac3cc84a1346913ede5d9bdda3dd5a7c071dd762
Safiya George <safiyageorge@Safiyas-MacBook-Pro.local>
2023-11-06 21:28:56 -0500

    I modified these scss files and added the javascript files so that the header is functional amd styled properly correctly in the mobile size.

---
+++ script.js
@@ -0,0 +1,34 @@
+const mainNav = document.getElementById('nav_bar');
+const toggleNav = document.getElementById('hamburger');
+const mainSec = document.getElementById('mainContent');
+
+toggleNav.addEventListener('click', (e) =>{
+  e.preventDefault();
+  const visibility = mainNav.getAttribute('data-visible');
+  if(visibility === "false"){
+    mainNav.setAttribute('data-visible', true);
+    toggleNav.setAttribute('aria-expanded', true);
+  }else{
+    mainNav.setAttribute('data-visible', false);
+    toggleNav.setAttribute('aria-expanded', false);
+  }
+ 
+});
+mainSec.addEventListener('click', (e) =>{
+  e.preventDefault();
+  const visibility = mainNav.getAttribute('data-visible');
+  if(visibility === 'true'){
+    mainNav.setAttribute('data-visible', false);
+    toggleNav.setAttribute('aria-expanded', false);
+  }
+})
+
+//the block below stops the animation of the navbar sliding when the page is being resized.
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
