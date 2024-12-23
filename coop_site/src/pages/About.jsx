8ec11054069f19ec52d89b79884c1790c4c12122
SafiyaMGeorge <smgdev1@outlook.com>
2024-12-23 14:38:37 -0500

    This is an image of sculptor

--- coop_site/src/pages/About.jsx
+++ coop_site/src/pages/About.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import './About.css'
 export default function About() {
   return (
-    <div>About</div>
+    <div id='containter'>
+      <section id='page_header'></section>
+      <main id='main content'>
+        <section id='sculpter_Img'>
+          <div id='img_Box'>
+            <img src="" alt="" />
+          </div>
+        </section>
+        <h1>I'm Shakim Cooper</h1>
+        <p id='bio'></p>
+      </main>
+    </div>
   )
 }
