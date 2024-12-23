399afad5dd345a2f4f6375853d7966402a41ed46
SafiyaMGeorge <smgdev1@outlook.com>
2024-12-23 15:50:21 -0500

    Added the content for the page.

--- coop_site/src/pages/About.jsx
+++ coop_site/src/pages/About.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './About.css'
+import Header from '../components/Header'
 export default function About() {
   return (
     <div id='containter'>
-      <section id='page_header'></section>
+      <header>
+        <Header />
+      </header>
       <main id='main content'>
         <section id='sculpter_Img'>
           <div id='img_Box'>
-            <img src="" alt="" />
+            <img src="../media/about_img.svg" alt="Creator of the sculptures." />
           </div>
         </section>
         <h1>I'm Shakim Cooper</h1>
