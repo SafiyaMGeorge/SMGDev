81c8ae3a9a578c6804ebd39d264b63c7456cd67e
SafiyaMGeorge <smgdev1@outlook.com>
2025-01-07 22:56:22 -0500

    added logo to header component.

--- coop_site/src/components/Header.jsx
+++ coop_site/src/components/Header.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import logo from '../media/Logo.png'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <section id='logo'>
+        <img src={logo} alt='logo' />
+      </section>
+      <section id='navburger'>menu</section>
+    </div>
   )
 }
