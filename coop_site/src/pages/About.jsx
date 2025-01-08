a44d59329c93a6576ce4d318d9d2a9b887248ba3
SafiyaMGeorge <smgdev1@outlook.com>
2025-01-07 22:39:36 -0500

    Change div inside containerid to overlay because this dive will give a gradient overlay of the background image to make the text more readable.

--- coop_site/src/pages/About.jsx
+++ coop_site/src/pages/About.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import './About.css'
 import Header from '../components/Header'
+
 export default function About() {
   return (
-    <div id='containter'>
-      <header>
+    <div id='container'>
+      <div id="overlay">
         <Header />
-      </header>
-      <main id='main content'>
-        <section id='sculpter_Img'>
-          <div id='img_Box'>
-            <img src="../media/about_img.svg" alt="Creator of the sculptures." />
-          </div>
-        </section>
-        <h1>I'm Shakim Cooper</h1>
-        <p id='bio'></p>
-      </main>
+      </div>
     </div>
   )
 }
