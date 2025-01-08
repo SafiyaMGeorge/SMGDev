0ecf5fb8212cb72e236a5d6cf9e3a5e688371832
SafiyaMGeorge <smgdev1@outlook.com>
2025-01-07 22:44:37 -0500

    move about style to its own folder.

--- coop_site/src/App.jsx
+++ coop_site/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Welcome from './pages/Welcome';
+import About from './pages/About';
 
 function App() {
   return (
     <BrowserRouter>
+    <div>
       <Routes>
         <Route index element={<Welcome />}/>
+        <Route path="/About" element={<About />}/>
       </Routes>
+    </div>
     </BrowserRouter>
   );
 }
