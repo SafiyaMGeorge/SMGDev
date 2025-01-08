b2e56e3f9368210c0ce3c2bc5d37547403c62926
SafiyaMGeorge <smgdev1@outlook.com>
2025-01-07 23:03:19 -0500

    Added hamburger menu icon.

--- coop_site/src/components/Header.jsx
+++ coop_site/src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import logo from '../media/Logo.png'
-
+import menu from '../media/menu.svg'
 export default function Header() {
   return (
     <div>
       <section id='logo'>
         <img src={logo} alt='logo' />
       </section>
-      <section id='navburger'>menu</section>
+      <section id='navburger'>
+        <img src={menu} alt="" />
+      </section>
     </div>
   )
 }
