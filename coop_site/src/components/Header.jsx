f92fdb2f92a59321618a716571d8e2ed6fec44ce
SafiyaMGeorge <smgdev1@outlook.com>
2025-01-09 00:14:59 -0500

    Added import of style page for components and giving the first div and id of container.

--- coop_site/src/components/Header.jsx
+++ coop_site/src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import logo from '../media/Logo.png'
 import menu from '../media/menu.svg'
+import './Header.css'
 export default function Header() {
   return (
-    <div>
+    <div id='container'>
       <section id='logo'>
         <img src={logo} alt='logo' />
       </section>
       <section id='navburger'>
-        <img src={menu} alt="" />
+        <img src={menu} alt="menu" />
       </section>
     </div>
   )
