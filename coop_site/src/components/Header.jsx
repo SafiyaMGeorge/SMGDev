b8cd69bc44f5dec1b1fb0e2413c8596df7d0b817
SafiyaMGeorge <smgdev1@outlook.com>
2025-01-13 17:26:47 -0500

    added code to import component.

--- coop_site/src/components/Header.jsx
+++ coop_site/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import logo from '../media/Logo.png'
-import menu from '../media/menu.svg'
+import Menu from './site_Nav'
 import './Header.css'
 export default function Header() {
   return (
     <div id='header_container'>
       <section id='logo'>
         <img src={logo} alt='logo' />
+      </section>
+      <section id='navburger'>
+        <Menu />
       </section>
     </div>
   )
