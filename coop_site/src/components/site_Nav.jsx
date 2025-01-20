86a1195585a63c729390d731afdcf8ac18ee3a0c
SafiyaMGeorge <smgdev1@outlook.com>
2025-01-20 18:50:01 -0500

    This is the base structure for the navigation of the site.

--- coop_site/src/components/site_Nav.jsx
+++ coop_site/src/components/site_Nav.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import menu from '../media/menu.svg'
 export default function site_Nav() {
   return (
     <div>
-       {/* <button>
-          <img src={menu} alt="menu" />
-        </button>
-        */}
+      <nav>
+        <ul>
+          
+          <li className='navItem'>
+            <Link to="/about" style={{textDecoration:'none'}}>About</Link>
+          </li>
+          <li className='navItem'>
+            <Link to="/gallery" style={{textDecoration:'none'}}>Gallery</Link>
+          </li>
+          <li className='navItem'>
+            <Link to="/commisions" style={{textDecoration:'none'}}>Commisions</Link>
+          </li>
+          <li className='navItem'>
+            <Link to="/contact" style={{textDecoration:'none'}}>Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
