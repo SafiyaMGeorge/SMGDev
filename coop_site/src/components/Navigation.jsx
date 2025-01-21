a3de377e75a91fd5752fca4d1dd004e0341cd44f
SafiyaMGeorge <smgdev1@outlook.com>
2025-01-20 21:15:28 -0500

    Created and imported css for navigation component.

--- coop_site/src/components/Navigation.jsx
+++ coop_site/src/components/Navigation.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import './Navigation.css'
 
 export default function site_Nav() {
   return (
