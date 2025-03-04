cc7aa6261fea3b27cf017ad5479c4b956c5a9058
SafiyaMGeorge <smgdev1@outlook.com>
2025-03-03 22:02:38 -0500

    Remove hard coded galley content and place a component in the page so that content can be dynamically loaded.

--- coop_site/src/pages/Gallery.jsx
+++ coop_site/src/pages/Gallery.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import GalleryCard from '../components/Gallery_Card' 
+import'./Gallery.css'
+import Header from '../components/Header'
 export default function Gallery() {
   return (
-    <div>Gallery</div>
+    <div id='container'>
+      <div id="overlay">
+        <Header />
+        <main id='gallery_section'>
+          <section id='prev_btn'>
+            <button>prev</button>
+          </section>
+          <section id='gallery_content'>
+            <GalleryCard />
+          </section>
+          <section id='next_btn'>
+            <button>next</button>
+          </section>
+        </main>
+      </div>
+    </div>
   )
 }
