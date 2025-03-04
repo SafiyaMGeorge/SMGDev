1fabb3b1493ecace70deaa157915239665acb826
SafiyaMGeorge <smgdev1@outlook.com>
2025-03-03 22:03:44 -0500

    Created this component so that content can be added as artwork is added to site.

---
+++ coop_site/src/components/Gallery_Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import plant1 from '../media/plant_img.1.jpg'
+import plant2 from '../media/plant_img.2.png'
+import plant3 from '../media/plant_img.3.jpg'
+import plant4 from '../media/plant_img.4.jpg'
+import plant5 from '../media/plant_img.5.png'
+import plant6 from '../media/plant_img.6.png'
+
+export default function Gallery_Display() {
+  return (
+    <div className='card'>
+      <section className='img_Container'>
+        
+      </section>
+      <section className='img_Title'></section>
+    </div>
+  )
+}
